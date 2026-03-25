5bd424fd9e2823b80477f9d89368edc3cc0e0125
sum123-run <sumairakiran35@gmail.com>
2026-03-24 21:46:46 -0700

    Add files via upload
    Code of Temperature Convertor

---
+++ mini_projects/SmartCalculator/temperature.py
@@ -0,0 +1,11 @@
+def c_to_f(c):
+    
+    temp_in_f = 9/5 *c+32
+    print(" temp_in_f is :", temp_in_f)
+    return  temp_in_f
+
+def f_to_c(f):
+    
+     temp_in_c = (f-32)*5/9
+     print(" temp_in_c is :", temp_in_c)
+     return  temp_in_c
