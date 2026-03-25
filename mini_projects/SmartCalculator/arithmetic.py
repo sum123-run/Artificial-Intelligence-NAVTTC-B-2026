71cb72b65fe3225a89ad5b5252276692d06faf24
sum123-run <sumairakiran35@gmail.com>
2026-03-24 21:44:15 -0700

    Add files via upload
    Code of arithmetic calculator

---
+++ mini_projects/SmartCalculator/arithmetic.py
@@ -0,0 +1,24 @@
+def add(a,b):
+    
+    addition=a+b
+    print(addition)
+    return addition
+    
+
+def subtract(a,b):
+    
+    subtraction=a-b
+    print(subtraction)
+    return subtraction
+    
+def multiply(a,b):
+    
+    multiplication=a*b
+    print(mutiplication)
+    return multiplication
+def divide(a,b):
+    
+    division=a/b
+    print(division)
+    return division
+    
