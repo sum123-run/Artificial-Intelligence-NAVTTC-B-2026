75d3173c5d4db4112fe54bf7b221f37012b3f27a
sum123-run <sumairakiran35@gmail.com>
2026-03-24 21:45:35 -0700

    Add files via upload
    Code of Length Convertor

---
+++ mini_projects/SmartCalculator/length.py
@@ -0,0 +1,28 @@
+def meters_to_km(m):
+    
+    len_km = (m /1000)
+    print("len_km is :",len_km)
+    return len_km
+
+def km_to_meters(km):
+    
+    len_m = (km *1000)
+    print("len_m is :",len_m)
+    return len_m
+    
+def meters_to_feet(m):
+    
+     j=3.28084
+     len_feet = (m*j)
+     print("len_feet is :",len_feet)
+     return len_feet
+    
+    
+def feet_to_meters(f):
+    
+    j=3.28084
+    len_meters = (f/j)
+    print("len_meters is :",len_meters)
+    return len_meters
+    
+    
